f6ffc60029bdde106201b3790a77b30c8ecb8603
leuisken <leuisken@gmail.com>
2016-03-03 19:22:52 +0800

    up the function

--- src/ep1.js
+++ src/ep1.js
@@ -1,5 +1,37 @@
 'use strict'
 
+/**
+ * 将一个没有层级的扁平对象,转换为树形结构({value, children})结构的对象
+ * @param {array} tableData - 一个由对象构成的数组,里面的对象都是扁平的
+ * @param {array} route - 一个由字符串构成的数组,字符串为前一数组中对象的key,最终
+ * 输出的对象层级顺序为keys中字符串key的顺序
+ * @return {array} 保存具有树形结构的对象
+ */
+
+var transObject = function(tableData, keys) {
+  let hashTable = {}, res = []
+  for( let i = 0; i < tableData.length; i++ ) {
+    if(!hashTable[tableData[i][keys[0]]]) {
+      let len = res.push({
+        value: tableData[i][keys[0]],
+        children: []
+      })
+      hashTable[tableData[i][keys[0]]] = { $$pos: len - 1 }
+    }
+    if(!hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]]) {
+      let len = res[hashTable[tableData[i][keys[0]]].$$pos].children.push({
+        value: tableData[i][keys[1]],
+        children: []
+      })
+      hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]] = { $$pos: len - 1 }
+    }
+    res[hashTable[tableData[i][keys[0]]].$$pos].children[hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]].$$pos].children.push({
+      value: tableData[i][keys[2]]
+    })
+  }
+  return res
+}
+
 var tableData = [{
   "name": "推荐",
   "action": "滑动频道页",
@@ -447,37 +479,5 @@ var tableData = [{
 }]
 
 var keys = ['category', 'action', 'name']
-
-/**
- * 将一个没有层级的扁平对象,转换为树形结构({value, children})结构的对象
- * @param {array} tableData - 一个由对象构成的数组,里面的对象都是扁平的
- * @param {array} route - 一个由字符串构成的数组,字符串为前一数组中对象的key,最终
- * 输出的对象层级顺序为keys中字符串key的顺序
- * @return {array} 保存具有树形结构的对象
- */
-
-var transObject = function(tableData, keys) {
-  let hashTable = {}, res = []
-  for( let i = 0; i < tableData.length; i++ ) {
-    if(!hashTable[tableData[i][keys[0]]]) {
-      let len = res.push({
-        value: tableData[i][keys[0]],
-        children: []
-      })
-      hashTable[tableData[i][keys[0]]] = { $$pos: len - 1 }
-    }
-    if(!hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]]) {
-      let len = res[hashTable[tableData[i][keys[0]]].$$pos].children.push({
-        value: tableData[i][keys[1]],
-        children: []
-      })
-      hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]] = { $$pos: len - 1 }
-    }
-    res[hashTable[tableData[i][keys[0]]].$$pos].children[hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]].$$pos].children.push({
-      value: tableData[i][keys[2]]
-    })
-  }
-  return res
-}
 
 console.log(transObject(tableData, keys))
