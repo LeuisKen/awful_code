29c0573ba8a49a4bc262bd8d95f67d74cea1a458
leuisken <leuisken@gmail.com>
2016-03-03 19:20:39 +0800

    add transObject

--- src/ep1.js
+++ src/ep1.js
@@ -1,331 +1,483 @@
-let columns,
-    dataSource,
-    lineChartConfig,
-    dimensions,
-    dimenActions,
-    type
+'use strict'
 
-if(!exp) {
-  type = 'main'
-} else if(!cate && target === '分类') {
-  type = 'category'
-} else if(!cate && target === '自定义事件') {
-  type = 'event'
-} else if(target === '位置') {
-  type = 'position'
-} else if(target === '类型') {
-  type = 'type'
+var tableData = [{
+  "name": "推荐",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "推荐",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "推荐",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "推荐",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "新闻",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "新闻",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "新闻",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "搞笑",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "推荐",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "新闻",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "美女",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "推荐",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "视频",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "新闻",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "推荐",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "美女",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "美女",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "搞笑",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "搞笑",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "搞笑",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "推荐",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "推荐",
+  "action": "点击Tab",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "视频",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "视频",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "小说",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "小说",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "美女",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "小说",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "搞笑",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "美女",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "视频",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "新闻",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "小说",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "视频",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "推荐",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "小说",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "新闻",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "搞笑",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "美女",
+  "action": "点击Tab",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "新闻",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "美女",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "新闻",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "新闻",
+  "action": "点击Tab",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "美女",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "搞笑",
+  "action": "点击Tab",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "搞笑",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "小说",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "视频",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "小说",
+  "action": "点击Tab",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "视频",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "视频",
+  "action": "点击Tab",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "视频",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "小说",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "美女",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "推荐",
+  "action": "下拉刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "视频",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "小说",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "美女",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "搞笑",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "搞笑",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "小说",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "推荐",
+  "action": "点击底部刷新按钮",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "新闻",
+  "action": "下拉刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "好物",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载更多"
+}, {
+  "name": "好物",
+  "action": "点击",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "Tab"
+}, {
+  "name": "好物",
+  "action": "下拉频道刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "美女",
+  "action": "下拉刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "好物",
+  "action": "点击底部按钮刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "好物",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "加载到底"
+}, {
+  "name": "视频",
+  "action": "下拉刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}, {
+  "name": "好物",
+  "action": "点击back键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "好物",
+  "action": "滑动频道页",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "进入信息流"
+}, {
+  "name": "好物",
+  "action": "点击home键",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "退出"
+}, {
+  "name": "搞笑",
+  "action": "下拉刷新",
+  "id": 0,
+  "day": "2016-03-02",
+  "category": "刷新"
+}]
+
+var keys = ['category', 'action', 'name']
+
+/**
+ * 将一个没有层级的扁平对象,转换为树形结构({value, children})结构的对象
+ * @param {array} tableData - 一个由对象构成的数组,里面的对象都是扁平的
+ * @param {array} route - 一个由字符串构成的数组,字符串为前一数组中对象的key,最终
+ * 输出的对象层级顺序为keys中字符串key的顺序
+ * @return {array} 保存具有树形结构的对象
+ */
+
+var transObject = function(tableData, keys) {
+  let hashTable = {}, res = []
+  for( let i = 0; i < tableData.length; i++ ) {
+    if(!hashTable[tableData[i][keys[0]]]) {
+      let len = res.push({
+        value: tableData[i][keys[0]],
+        children: []
+      })
+      hashTable[tableData[i][keys[0]]] = { $$pos: len - 1 }
+    }
+    if(!hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]]) {
+      let len = res[hashTable[tableData[i][keys[0]]].$$pos].children.push({
+        value: tableData[i][keys[1]],
+        children: []
+      })
+      hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]] = { $$pos: len - 1 }
+    }
+    res[hashTable[tableData[i][keys[0]]].$$pos].children[hashTable[tableData[i][keys[0]]][tableData[i][keys[1]]].$$pos].children.push({
+      value: tableData[i][keys[2]]
+    })
+  }
+  return res
 }
 
-columns = getColumns(type)
-if(type === 'main') {
-  dimensions = '实验'
-  columns[0].render = function(text, record) {
-    return (
-      <span className="linkArea">
-        <Link className="pull-right mr30" to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              key: record.eid
-            }
-          }}>
-          <i className="fa fa-line-chart fa-15"></i>
-        </Link>
-        <Link to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              exp: record.eid,
-              target: '分类',
-              key: 'total'
-            }
-          }}>
-          {text}
-        </Link>
-      </span>
-    )
-  }
-  columns[0].width = '156'
-  columns[8].width = '131'
-
-} else if(type === 'category') {
-  dimensions = ['分类', '自定义事件']
-  let cateAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      target: '分类',
-      key: 'total'
-    }
-  }
-  let eventAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      target: '自定义事件',
-      key: 'total'
-    }
-  }
-  dimenActions = [cateAction, eventAction]
-  columns[0].render = function(text, record) {
-    return (
-      <span className="linkArea">
-        <Link className="pull-right mr30" to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              exp: record.eid,
-              target: '分类',
-              key: record.category
-            }
-          }}>
-          <i className="fa fa-line-chart fa-15"></i>
-        </Link>
-        <Link to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              exp: record.eid,
-              target: '位置',
-              cate: record.category,
-              key: '1'
-            }
-          }}>
-          {text}
-        </Link>
-      </span>
-    )
-  }
-
-} else if(type === 'event') {
-  dimensions = ['分类', '自定义事件']
-  let cateAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      target: '分类',
-      key: 'total'
-    }
-  }
-  let eventAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      target: '自定义事件',
-      key: 'total'
-    }
-  }
-  dimenActions = [cateAction, eventAction]
-  columns[2].render = function(text, record) {
-    return (
-      <span className="linkArea">
-        <Link className="pull-right mr30" to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              exp: record.eid,
-              target: '自定义事件',
-              key: JSON.stringify({
-                category: record.category,
-                action: record.action,
-                name: record.name
-              })
-            }
-          }}>
-          <i className="fa fa-line-chart fa-15"></i>
-        </Link>
-        {text}
-      </span>
-    )
-  }
-
-} else if(type === 'position') {
-  dimensions = ['位置', '类型']
-  let posAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      cate: cate,
-      target: '位置',
-      key: '1'
-    }
-  }
-  let typeAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      cate: cate,
-      target: '类型',
-      key: 'News'
-    }
-  }
-  dimenActions = [posAction, typeAction]
-  columns[0].render = function(text, record) {
-    return (
-      <span className="linkArea">
-        <Link className="pull-right mr30" to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              exp: record.eid,
-              cate: record.category,
-              target: '位置',
-              key: record.position
-            }
-          }}>
-          <i className="fa fa-line-chart fa-15"></i>
-        </Link>
-        {text}
-      </span>
-    )
-  }
-
-} else if(type === 'type') {
-  dimensions = ['位置', '类型']
-  let posAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      cate: cate,
-      target: '位置',
-      key: '1'
-    }
-  }
-  let typeAction = {
-    pathname: '/mi-life/experiment/offline',
-    query: {
-      exp: exp,
-      cate: cate,
-      target: '类型',
-      key: 'News'
-    }
-  }
-  dimenActions = [posAction, typeAction]
-  columns[0].render = function(text, record) {
-    return (
-      <span className="linkArea">
-        <Link className="pull-right mr30" to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              exp: record.eid,
-              cate: record.category,
-              target: '类型',
-              key: record.position
-            }
-          }}>
-          <i className="fa fa-line-chart fa-15"></i>
-        </Link>
-        {text}
-      </span>
-    )
-  }
-}
-
-if(table) {
-  dataSource = getTableResult(table.result, type)
-  var csvData = getTableCSVData(dataSource, type)
-  var csvList = new Blob([Papa.unparse(csvData)], {type: "text/csv;charset=utf-8"})
-  var listFileName  = '实验' + moment(tableTs).format('YYYY.MM.DD') + '.csv'
-}
-
-if(type !== 'main' && chart) {
-  lineChartConfig = getLineChartData(chart.result, type)
-  var csvLineData = getLineCSVData(chart.result, type)
-  var csvLine = new Blob([Papa.unparse(csvLineData)], {type: "text/csv;charset=utf-8"})
-  var lineFileName = key + '-' + moment(startTs).format('YYYY.MM.DD') +
-    '-' + moment(endTs).format('YYYY.MM.DD') + '.csv'
-} else if(chart && chart.result.pv) {
-  lineChartConfig = getLineChartData(chart.result, type)
-  var csvLineData = getLineCSVData(chart.result, type)
-  var csvLine = new Blob([Papa.unparse(csvLineData)], {type: "text/csv;charset=utf-8"})
-  var lineFileName = key + '-' + moment(startTs).format('YYYY.MM.DD') +
-    '-' + moment(endTs).format('YYYY.MM.DD') + '.csv'
-}
-
-let rowClassName = (record, index) => {
-  if(type === 'main') {
-    return record.eid === key ? "current": ""
-  } else if(type === 'category') {
-    return record.category === key ? "current": ""
-  } else if(type === 'event') {
-    let col = {
-      category: record.category,
-      action: record.action,
-      name: record.name
-    }
-    return JSON.stringify(col) === key ? "current": ""
-  } else {
-    return record.position === key ? "current": ""
-  }
-}
-
-let chartTitile = (() => {
-  if(type === 'main') {
-    return '离线实验'
-  } else if(type === 'category') {
-    return (
-      <span>
-        <Link to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              key: exp
-            }
-          }}>
-          离线实验
-        </Link> - 分类
-      </span>
-    )
-  } else if(type === 'event') {
-    return (
-      <span>
-        <Link to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              key: exp
-            }
-          }}>
-          离线实验
-        </Link> - 自定义事件
-      </span>
-    )
-  } else if(type === 'position') {
-    return (
-      <span>
-        <Link to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              key: exp
-            }
-          }}>
-          离线实验
-        </Link> - <Link to={{
-          pathname: '/mi-life/experiment/offline',
-          query: {
-            exp,
-            target: '分类',
-            key: cate
-          }
-        }}>
-          分类
-        </Link> - 位置
-      </span>
-    )
-  } else if(type === 'type') {
-    return (
-      <span>
-        <Link to={{
-            pathname: '/mi-life/experiment/offline',
-            query: {
-              key: exp
-            }
-          }}>
-          离线实验
-        </Link> - <Link to={{
-          pathname: '/mi-life/experiment/offline',
-          query: {
-            exp,
-            target: '分类',
-            key: cate
-          }
-        }}>
-          分类
-        </Link> - 位置
-      </span>
-    )
-  }
-})()
+console.log(transObject(tableData, keys))
